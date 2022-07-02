afba4479b1732c6d923b9251966606b364e0b0c8
Malvin Tok <93638561+malvintok@users.noreply.github.com>
2022-07-02 16:35:51 +0800

    🎨  Change folder structure and tidy code for prod

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'tachyons';
 
 import './index.css';
-import App from './App'
+import App from './containers/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
